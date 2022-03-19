192f96cbb619d3a3ddf96f3fcd2397ad6709d6f0
MimoCrew <100687394+MimoCrew@users.noreply.github.com>
2022-03-19 12:59:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const app = express();
+app.get('/', (req, res) =>{
+  res.send("Server 24/7 Start! Mimo sagt danke")
+})
+app.listen(3000, () => {
+  console.log("Server Online")
+})
+
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const Discord = require('discord.js');
+const statusmessages = ["m!help" , "Spielt auf deinem Server" , "Ja genau lol"]
+current = [0]
+
+const prefix = "m!"
+
+client.on('ready', () => {
+  console.log("Eingeloggt als "+client.user.tag+"! Auf "+client.guilds.cache.size+" Servern")
+  client.user.setActivity(statusmessages[0] , {type:"PLAYING"})
+    setInterval(() => {
+      if (statusmessages[current]){
+          client.user.setActivity(statusmessages[current] , {type:"PLAYING"})
+          current++
+      }else{
+          current = 0
+          client.user.setActivity(statusmessages[current] , {type:"PLAYING"})
+      }
+    }, 5*1000)
+    client.user.setPresence({
+        status: "dnd"
+  })
+})
+
+client.on("messageCreate", async message => {
+  if(message.content == "hallo"){
+	message.channel.send("Hallo "+message.author.username)
+  }
+  if(message.content == prefix+"botinfo"{
+	message.channel.send("Hier ist die Botinfo:\nDeveloped by hyped.lukas\n Ping: "+client.ws.ping)
+   }
+  if(message.content.startsWith(prefix+"help")){
+    message.channel.send("Hier ist die Hilfe: \nCommands coming soon)
+   }
+})
+
+client.login("Dein-Bot-Token")
